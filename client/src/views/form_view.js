7bd9fb98cd6707bdc078e725c960253d68a91353
Alex-Lakin <alex.lakin@hotmail.co.uk>
2018-06-03 12:01:11 +0100

    can send values

--- client/src/views/form_view.js
+++ client/src/views/form_view.js
@@ -8,18 +8,24 @@ FormView.prototype.bindEvents = function () {
   PubSub.subscribe('co2Collection:data-loaded', (event) => {
     // populate form with data from api
     const carField = document.querySelector('#car');
-    carField.value = event.detail[0].car;
+    const carValues = event.detail[0].car;
+    carField.value = carValues;
     const trainField = document.querySelector('#train');
-    trainField.value = event.detail[0].train;
+    const trainValues = event.detail[0].train;
+    trainField.value = trainValues
 
     const submitButton = document.querySelector('#submit');
     submitButton.addEventListener('click', (event) => {
       event.preventDefault();
-      // this.sendFormToAPI(event);
+
+      this.sendFormToAPI(carValues, trainValues);
     });
   });
 };
 
-// FormView.prototype.sendFormToAPI =
+FormView.prototype.sendFormToAPI = function(car,train) {
+  console.log(car);
+  console.log(train);
+}
 
 module.exports = FormView;
