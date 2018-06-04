855fbbf712784ffbdb4e09f6905fd7d9257ef346
Shoogaboog <lewisjohnmclean@gmail.com>
2018-06-04 10:39:22 +0100

    Added field for recycling.

--- client/src/views/form_view.js
+++ client/src/views/form_view.js
@@ -14,13 +14,17 @@ FormView.prototype.bindEvents = function () {
     const trainValues = event.detail[0].train;
     const planeField = document.querySelector('#plane');
     const planeValues = event.detail[0].plane;
+    const recycleField = document.querySelector('input[name="recycle"]:checked');
+    const recycleValues = event.detail[0].value;
     const meatDropdown = document.querySelector('#meatDropdown');
     const meatValues = event.detail[0].meat;
 
     trainField.value = trainValues;
     carField.value = carValues;
     planeField.value = planeValues;
+    recycleField.value = recycleValues;
     meatDropdown.value = meatValues;
+    console.log('recycleValues:', recycleValues);
     });
 
     this.form.addEventListener('submit', (evt) => {
@@ -28,17 +32,19 @@ FormView.prototype.bindEvents = function () {
       const carValues = evt.target.car.value;
       const trainValues = evt.target.train.value;
       const planeValues = evt.target.plane.value;
+      const recycleValues = evt.target.recycle.value;
       const meatValues = evt.target.meat.value;
 
-      this.sendFormToModel(carValues, trainValues, planeValues, meatValues);
+      this.sendFormToModel(carValues, trainValues, planeValues, recycleValues,  meatValues);
   });
 };
 
-FormView.prototype.sendFormToModel = function(carValues, trainValues, planeValues, meatValues) {
+FormView.prototype.sendFormToModel = function(carValues, trainValues, planeValues, recycleValues, meatValues) {
   const allData = {
       car: carValues,
       train: trainValues,
       plane: planeValues,
+      recycle: recycleValues,
       meat: meatValues
   };
   PubSub.publish('FormView:updated-data-ready', allData);
