da1d3eaa38b94efee5ddf3ad1dc2188d89bee16e
Marta Beveridge <marta@beveridge.uk>
2018-06-05 16:30:18 +0100

    Finished up some more CSS.

--- client/src/views/results_view.js
+++ client/src/views/results_view.js
@@ -11,6 +11,9 @@ ResultsView.prototype.getResults = function () {
     const displayableTotal = document.createElement('p');
     displayableTotal.textContent = `Your carbon footprint is ${evt.detail} tonnes a year.`;
     this.resultsContainer.appendChild(displayableTotal);
+    const ukAverage = document.createElement('p');
+    ukAverage.textContent = `The average UK citizen produces 76 tonnes.`
+    this.resultsContainer.appendChild(ukAverage);
   });
 };
 
